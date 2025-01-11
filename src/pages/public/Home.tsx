975dc48655d1f7cec2d2a8e102d97dc5f1484b1c
Choerul Sofyan <choerulsofyanmf@gmail.com>
2025-01-11 19:08:45 +0700

    minor changes

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -3,7 +3,6 @@ import React from "react";
 const Home: React.FC = () => {
     return (
         <div className="container mx-auto px-4 pt-20">
-            {/* Container for centering and padding */}
             <h1 className="text-3xl font-bold mb-4">Welcome to the Travel Article App</h1> {/* Heading */}
             <p className="text-gray-700 leading-relaxed w-3/4">
                 Welcome to our travel article platform. Browse insightful articles about destinations worldwide. Join our community to contribute your
