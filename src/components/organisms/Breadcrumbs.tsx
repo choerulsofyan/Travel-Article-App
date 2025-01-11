959ddd76dc5ff5f011fa8c9fb9ffe895cf1462c8
Choerul Sofyan <choerulsofyanmf@gmail.com>
2025-01-11 19:50:43 +0700

    minor changes

--- src/components/organisms/Breadcrumbs.tsx
+++ src/components/organisms/Breadcrumbs.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { paths } from "@/routes/paths";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
 
